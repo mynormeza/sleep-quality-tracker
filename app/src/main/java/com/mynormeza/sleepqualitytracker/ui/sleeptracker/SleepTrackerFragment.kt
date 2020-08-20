5bb13470d6161addb83b6ad235418ec543ce4aa5
Mynor Meza <mynormeza92@gmail.com>
2020-08-20 11:52:59 -0600

    add sleep night recycler

--- app/src/main/java/com/mynormeza/sleepqualitytracker/ui/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/mynormeza/sleepqualitytracker/ui/sleeptracker/SleepTrackerFragment.kt
@@ -30,6 +30,15 @@ class SleepTrackerFragment : Fragment() {
                 this, viewModelFactory).get(SleepTrackerViewModel::class.java)
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
+
         binding.lifecycleOwner = this
 
         sleepTrackerViewModel.showSnackBarEvent.observe(viewLifecycleOwner, Observer {
